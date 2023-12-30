1f83657c50c364965d56604aab9ef81a4d311007
Vaibhav Singh <vaibhavsinghthebest@gmail.com>
2023-12-30 12:50:35 +0530

    first commit

---
+++ src/WebImage.jsx
@@ -0,0 +1,45 @@
+
+import Webcam from "react-webcam";
+import React, { useState, useRef, useCallback } from "react";
+
+function WebImage() {
+  const webcamRef = useRef(null);
+  const [img, setImg] = useState(null);
+
+const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImg(imageSrc);
+  }, [webcamRef]);
+
+  const videoConstraints = {
+    width: 390,
+    height: 390,
+    facingMode: "user",
+  };
+
+  return (
+    <div className="Container">
+      {img === null ? (
+        <>
+          <Webcam
+            screenshotFormat="image/jpeg"
+            videoConstraints={videoConstraints}
+            audio={false}
+            height={500}
+            width={500}
+            ref={webcamRef}
+            mirrored={true}
+          />
+          <button onClick={capture}>Capture photo</button>
+        </>
+      ) : (
+        <>
+          <img src={img} alt="screenshot" />
+          <button onClick={() => setImg(null)}>Recapture</button>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default WebImage;
